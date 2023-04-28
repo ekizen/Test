65d1df3d105de35ce049473b49558b0e65962af2
Leonilo Tegerero <42099501+ekizen@users.noreply.github.com>
2023-04-27 23:52:34 -0400

    Update login_steps.py
    Added Wait on page load.

--- OrangeHRM_Test/steps/login_steps.py
+++ OrangeHRM_Test/steps/login_steps.py
@@ -1,10 +1,18 @@
 from behave import *
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 @given('I am on the login page')
 def step_impl(context):
     context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
     context.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    wait = WebDriverWait(context.driver, 30)
+    element = wait.until(EC.presence_of_element_located(
+        (By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div/div[1]/img')))
+    assert True == element.is_displayed()
 
 @when('I enter "{username}" as username')
 def step_impl(context, username):
